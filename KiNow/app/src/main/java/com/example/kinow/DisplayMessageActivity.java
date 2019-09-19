2fa0a7ae606cd427c0100a9835c1185241811a51
Lukas Blank <Lukas0704@web.de>
2019-09-19 09:22:52 +0200

    rename second acitivity

--- KiNow/app/src/main/java/com/example/kinow/DisplayMessageActivity.java
+++ KiNow/app/src/main/java/com/example/kinow/DisplayMessageActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SecondaryActivity extends AppCompatActivity {
+public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_secondary);
+        setContentView(R.layout.activity_display_message);
     }
 }
