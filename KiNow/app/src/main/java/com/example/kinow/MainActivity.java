f46182d9ad7336447e8459db494e9305dae103f8
unknown <Lukas0704@web.de>
2019-09-22 17:09:58 +0200

    implementing firebase

--- KiNow/app/src/main/java/com/example/kinow/MainActivity.java
+++ KiNow/app/src/main/java/com/example/kinow/MainActivity.java
@@ -9,11 +9,6 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
 
-import org.postgresql.Driver;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = ""; //Variable die beim Wechseln der Ansicht übergeben wird
@@ -22,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        connect();
     }//onCreate
 
     public void sendMessage (View view) {
@@ -32,22 +26,6 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE , message);
         startActivity(intent);
     }//sendMessage
-
-    private void connect (){
-        try {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-            Class.forName("org.postgresql.Driver");
-            Connection c = null;
-            c = DriverManager.getConnection("jdbc:postgresql://10.0.2.2:5432/KiNow","postgres","postgres");
-            //^^ Versuch mit Datenbank zu verbinden
-            EditText e = (EditText) findViewById(R.id.Textfeld);
-            e.setText("EASY");
-        } catch (Exception e){
-            EditText ex = (EditText) findViewById(R.id.Textfeld);
-            ex.setText(e.fillInStackTrace().toString());
-        }
-    }//connect
 
 
 }//class
