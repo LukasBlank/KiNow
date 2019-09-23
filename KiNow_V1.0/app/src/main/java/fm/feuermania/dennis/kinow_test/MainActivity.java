de438cc1bde2cda1b693077249cb576db4c2521d
unknown <Lukas0704@web.de>
2019-09-23 17:05:09 +0200

    update

--- KiNow_V1.0/app/src/main/java/fm/feuermania/dennis/kinow_test/MainActivity.java
+++ KiNow_V1.0/app/src/main/java/fm/feuermania/dennis/kinow_test/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 //import com.roughike.bottombar.BottomBar;
--- KiNow_V1.0/app/src/main/java/fm/feuermania/dennis/kinow_test/SearchFragment.java
+++ KiNow_V1.0/app/src/main/java/fm/feuermania/dennis/kinow_test/SearchFragment.java
@@ -3,10 +3,18 @@ package fm.feuermania.dennis.kinow_test;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 /**
@@ -22,6 +30,7 @@ public class SearchFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -54,6 +63,7 @@ public class SearchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -105,4 +115,9 @@ public class SearchFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    public void test (View view){
+        ;
+    }
+
 }
