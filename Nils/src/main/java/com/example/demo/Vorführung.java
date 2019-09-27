0bf9b84c9796916a94a9e2963aac2ffb15f15c19
Lukas Blank <Lukas0704@web.de>
2019-09-27 23:20:30 +0200

    update

--- Nils/src/main/java/com/example/demo/Vorführung.java
+++ Nils/src/main/java/com/example/demo/Vorführung.java
@@ -4,30 +4,58 @@ import java.util.Date;
 
 public class Vorführung {
 
-    int VID,FID,SaalID;
-    Date zeitpunk;
+    int vid,filmID,saalID,gesamtdauer,preis,freieSitze;
+    Date zeitpunkt;
+    boolean dreiD;
 
 
-    public Vorführung(int VID, int FID, int saalID, Date zeitpunk) {
-        this.VID = VID;
-        this.FID = FID;
-        SaalID = saalID;
-        this.zeitpunk = zeitpunk;
+
+    public Vorführung(int vid, int filmID, int saalID, int preis, boolean dreiD, Date zeitpunkt) {
+        this.vid = vid;
+        this.filmID = filmID;
+        this.saalID = saalID;
+        this.preis = preis;
+        this.zeitpunkt = zeitpunkt;
+        this.dreiD = dreiD;
+    }//Konstruktor
+
+    public void setGesamtdauer(int gesamtdauer) {
+        this.gesamtdauer = gesamtdauer;
     }
 
-    public int getVID() {
-        return VID;
+    public void setFreieSitze(int freieSitze) {
+        this.freieSitze = freieSitze;
     }
 
-    public int getFID() {
-        return FID;
+    public int getVid() {
+        return vid;
+    }
+
+    public int getFilmID() {
+        return filmID;
     }
 
     public int getSaalID() {
-        return SaalID;
+        return saalID;
     }
 
-    public Date getZeitpunk() {
-        return zeitpunk;
+    public int getGesamtdauer() {
+        return gesamtdauer;
     }
-}
+
+    public int getPreis() {
+        return preis;
+    }
+
+    public int getFreieSitze() {
+        return freieSitze;
+    }
+
+    public Date getZeitpunkt() {
+        return zeitpunkt;
+    }
+
+    public boolean isDreiD() {
+        return dreiD;
+    }
+}//class
