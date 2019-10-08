77b263d540c86b806fa238e26aadb3b293c57c8d
Lukas Blank <Lukas0704@web.de>
2019-10-08 10:37:58 +0200

    copy kinow_test_akt

---
+++ lukas/app/src/main/java/fm/feuermania/dennis/kinow_test/LocationAdapter.java
@@ -0,0 +1,58 @@
+package fm.feuermania.dennis.kinow_test;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHolder> {
+
+    ArrayList<String> location_list;
+    Context context;
+
+    public LocationAdapter(ArrayList<String> list, Context context) {
+        this.location_list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public LocationAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(context).inflate(R.layout.location_view, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LocationAdapter.ViewHolder viewHolder, int i) {
+        viewHolder.locTitle.setText(location_list.get(i).toString());
+        viewHolder.locDesc.setText(location_list.get(i).toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return location_list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView locImage;
+        public TextView locTitle, locDesc;
+        public RelativeLayout relLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            locImage  = itemView.findViewById(R.id.locImage);
+            locTitle     = itemView.findViewById(R.id.locTitle);
+            locDesc      = itemView.findViewById(R.id.locDesc);
+            relLayout = itemView.findViewById(R.id.relLayout);
+        }
+    }
+
+}
--- lukas/app/src/main/java/fm/feuermania/dennis/kinow_test/MainActivity.java
+++ lukas/app/src/main/java/fm/feuermania/dennis/kinow_test/MainActivity.java
@@ -1,23 +1,16 @@
 package fm.feuermania.dennis.kinow_test;
 
-import android.graphics.Color;
 import android.net.Uri;
-import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-//import com.roughike.bottombar.BottomBar;
-//import com.roughike.bottombar.OnMenuTabClickListener;
-
-public class MainActivity extends AppCompatActivity implements MoviesFragment.OnFragmentInteractionListener, ShoppingCartFragment.OnFragmentInteractionListener, SearchFragment.OnFragmentInteractionListener, AccountFragment.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements MoviesFragment.OnFragmentInteractionListener, ShoppingCartFragment.OnFragmentInteractionListener, LocationFragment.OnFragmentInteractionListener, AccountFragment.OnFragmentInteractionListener{
 
     //private BottomBar BottomBar;
     private ActionBar kinowToolbar;
@@ -39,45 +32,6 @@ public class MainActivity extends AppCompatActivity implements MoviesFragment.On
         navigation.setOnNavigationItemSelectedListener(botNavItemListener);
         navigation.setSelectedItemId(R.id.tab_movies);
 
-        /*
-        BottomBar = BottomBar.attach(findViewById(R.id.mainLayout), savedInstanceState);
-        BottomBar.noTopOffset();
-        BottomBar.setItems(R.menu.bot_nav_items);
-        BottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
-            @Override
-            public void onMenuTabSelected(@IdRes int menuItemId) {
-                switch (menuItemId) {
-                    case R.id.tab_search:
-                        Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.tab_movies:
-                        Toast.makeText(MainActivity.this, "Movies", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.tab_cart:
-                        Toast.makeText(MainActivity.this, "Purchases", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.tab_account:
-                        Toast.makeText(MainActivity.this, "Account", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-
-            @Override
-            public void onMenuTabReSelected(@IdRes int menuItemId) {
-                if (menuItemId == R.id.tab_search) {
-                    // The user reselected item number one, scroll your content to top.
-                }
-            }
-        });
-
-        // Setting colors for different tabs when there's more than three of them.
-        // You can set colors for tabs in three different ways as shown below.
-        BottomBar.mapColorForTab(0, ContextCompat.getColor(this, R.color.mint));
-        BottomBar.mapColorForTab(1, ContextCompat.getColor(this, R.color.brown));
-        BottomBar.mapColorForTab(2, ContextCompat.getColor(this, R.color.violet));
-        BottomBar.mapColorForTab(3, ContextCompat.getColor(this, R.color.red));
-         */
-
     }
 
     //Change Fragment depending on which Button is pressed
@@ -93,9 +47,9 @@ public class MainActivity extends AppCompatActivity implements MoviesFragment.On
                     fragment = new MoviesFragment();
                     loadFragment(fragment);
                     return true;
-                case R.id.tab_search:
-                    kinowToolbar.setTitle("Search");
-                    fragment = new SearchFragment();
+                case R.id.tab_location:
+                    kinowToolbar.setTitle("Location");
+                    fragment = new LocationFragment();
                     loadFragment(fragment);
                     return true;
                 case R.id.tab_cart:
