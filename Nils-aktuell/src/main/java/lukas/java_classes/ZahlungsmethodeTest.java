75c1b3b12268f8372c29e2d6a07e594e267234a3
T.B <meineEmail>
2019-10-14 17:04:17 +0200

    Werbung Test

--- Nils-aktuell/src/main/java/lukas/java_classes/ZahlungsmethodeTest.java
+++ Nils-aktuell/src/main/java/lukas/java_classes/ZahlungsmethodeTest.java
@@ -3,21 +3,19 @@ package main.java.lukas.java_classes;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class ZahlungsmethodeTest {
-    Zahlungsmethode testZahlungsMethode1 = new Zahlungsmethode(1,"Test_Methode");
-    Zahlungsmethode testZahlungsMethode2 = new Zahlungsmethode(1,"Test_Methode");
+    Zahlungsmethode testZahlungsMethode = new Zahlungsmethode(1,"Test_Methode");
 
     @Test
-    public void getMethodeID() { Assert.assertEquals(1,testZahlungsMethode1.getMethodeID()); }
+    public void getMethodeID() { Assert.assertEquals(1, testZahlungsMethode.getMethodeID()); }
 
     @Test
-    public void getMethode() { Assert.assertEquals("Test_Methode",testZahlungsMethode1.getMethode());
+    public void getMethode() { Assert.assertEquals("Test_Methode", testZahlungsMethode.getMethode());
     }
 
     @Test
-    public void equals() {
-        Assert.assertEquals(testZahlungsMethode1,testZahlungsMethode2);
+    public void equals(Zahlungsmethode zahlungsmethode) {
+        Assert.assertEquals(true,testZahlungsMethode.equals(testZahlungsMethode));
+        Assert.assertEquals(false,testZahlungsMethode.equals(new Zahlungsmethode(2,"andere Methode")));
     }
 }
