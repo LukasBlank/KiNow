4b6f7f005b4489fd0794720b0e47a5094448b478
Lukas Blank <Lukas0704@web.de>
2019-10-20 18:07:51 +0200

    updates

--- kinow_backend/app/src/main/java/frontend/MovieDetailScreen.java
+++ kinow_backend/app/src/main/java/frontend/MovieDetailScreen.java
@@ -1,21 +1,28 @@
 package frontend;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MovieDetailScreen extends AppCompatActivity {
+import backend.classes.Film;
+
+public class MovieDetailScreen extends AppCompatActivity{
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail_screen);
-    }
+    }//onCreate
 
     public void watchTrailer(View view) {
         Intent intent = new Intent(MovieDetailScreen.this, MovieTrailer.class);
         startActivity(intent);
     }
 
-}
+}//class
