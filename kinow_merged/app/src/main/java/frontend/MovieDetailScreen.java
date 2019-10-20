479d9a97205a6d785d5b54c39b86ce8bfce39262
Dennis Neumann <dennis2598@gmx.net>
2019-10-20 22:48:53 +0200

    Merged kinow_test with kinow_merged

--- kinow_merged/app/src/main/java/frontend/MovieDetailScreen.java
+++ kinow_merged/app/src/main/java/frontend/MovieDetailScreen.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import backend.classes.Film;
@@ -13,15 +12,58 @@ import backend.classes.Film;
 public class MovieDetailScreen extends AppCompatActivity{
 
     Context context;
+    TextView timeOne;
+    TextView timeTwo;
+    TextView timeThree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail_screen);
+
+        timeOne = findViewById(R.id.time_one);
+        timeTwo = findViewById(R.id.time_two);
+        timeThree = findViewById(R.id.time_three);
+
+        timeOne.setOnClickListener(onClickListener);
+        timeTwo.setOnClickListener(onClickListener);
+        timeThree.setOnClickListener(onClickListener);
     }//onCreate
+
+    // Dennis Notiz: Evtl. durch if Anweisung ersetzen, + Book Button sool nur gehen wenn eine der Zeiten ausgewählt ist
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()){
+
+                case R.id.time_one:
+                    timeOne.setBackgroundColor(getResources().getColor(R.color.red));
+                    timeTwo.setBackgroundColor(getResources().getColor(R.color.transparent));
+                    timeThree.setBackgroundColor(getResources().getColor(R.color.transparent));
+                    break;
+
+                case R.id.time_two:
+                    timeOne.setBackgroundColor(getResources().getColor(R.color.transparent));
+                    timeTwo.setBackgroundColor(getResources().getColor(R.color.red));
+                    timeThree.setBackgroundColor(getResources().getColor(R.color.transparent));
+                    break;
+
+                case R.id.time_three:
+                    timeOne.setBackgroundColor(getResources().getColor(R.color.transparent));
+                    timeTwo.setBackgroundColor(getResources().getColor(R.color.transparent));
+                    timeThree.setBackgroundColor(getResources().getColor(R.color.red));
+                    break;
+            }
+        }
+    };
 
     public void watchTrailer(View view) {
         Intent intent = new Intent(MovieDetailScreen.this, MovieTrailer.class);
+        startActivity(intent);
+    }
+
+    public void bookMovie(View view) {
+        Intent intent = new Intent(MovieDetailScreen.this, SmallCinemaHall.class);
         startActivity(intent);
     }
 
---
+++ kinow_merged/app/src/main/java/frontend/SmallCinemaHall.java
@@ -0,0 +1,22 @@
+package frontend;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SmallCinemaHall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.small_cinema_hall);
+    }
+
+    public void bookSeats(){
+
+        // Dennis Notiz: Mind. 1 Sitz muss ausgewählt sein
+        // ++ Sitze onClick: Farbe ändern, wenn nochmals geklickt wird -> Farbe wieder entfernen
+        // ++ Besetzte Sitze anzeigen
+
+    }
+
+}
