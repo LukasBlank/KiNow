d9f67765193504963c991a74c7ee3b30438e4260
T.B <meineEmail>
2019-10-22 03:31:40 +0200

    tobi tests finished

--- kinow_merged/app/src/test/java/tobi/BestellungTest.java
+++ kinow_merged/app/src/test/java/tobi/BestellungTest.java
@@ -12,6 +12,7 @@ import backend.classes.Preisvariation;
 import backend.classes.Sitz;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 
 public class BestellungTest {
@@ -55,22 +56,54 @@ public class BestellungTest {
     @Test
     public void setGesamtpreis(){
         //Buchung == null
+        Bestellung testbestellung2 = new Bestellung("bestellungsnummer", 1,null);
+        testbestellung2.setGesamtpreis();
+        if(testbestellung2.getBuchungen() == null){
+            assertNull(testbestellung2.getGesamtpreis());
+        }
         //Buchung != null
+        testBestellung.setGesamtpreis();
+        if(testBestellung.getBuchungen() != null){
+            Assert.assertEquals(2,testBestellung.getGesamtpreis());
+
+        }
     }
 
     @Test
     public void addBuchung(){
+        //!contains buchung2
+        Buchung buchung2 = new Buchung(1,2,"vorführungsID","buchungsID",new ArrayList<Sitz>(),new ArrayList<Preisvariation>());
+        if(!testbuchung1.contains(buchung2)){
+            testbuchung1.add(buchung2);
+            assertTrue(testbuchung1.contains(buchung2));
+        }
 
     }
 
     @Test
     public void removeBuchung(){
+        //contains buchung2
+        Buchung buchung2 = new Buchung(1,2,"vorführungsID","buchungsID",new ArrayList<Sitz>(),new ArrayList<Preisvariation>());
+        if(testbuchung1.contains(buchung2)){
+            testbuchung1.remove(buchung2);
+            assertFalse(testbuchung1.contains(buchung2));
+        }
+
 
     }
 
     @Test
     public void getNutzerID(){
-
+        //BuchungsID.length <=0
+        Buchung testbuchung1 = new Buchung(1,1,"vorführungsID","",null,null);
+        assertNull(testbuchung1.getBestellungsnummer());
+        //Buchung mit falscher indizierung der BuchungsID
+        Buchung testbuchung2 = new Buchung(1,1,"vorführungsID","notwendiges Zeichen taucht nicht auf",null,null);
+        assertNull(testbuchung2.getBestellungsnummer());
+        //die if.Bedingung wird erfüllt
+        Buchung testbuchung3 = new Buchung(1,1,"vorführungsID","_BuchungID_",null,null);
+        assertTrue(testbuchung3.getNutzerID().equals(testbuchung3.getNutzerID()));
+        
     }
 
     @Test
