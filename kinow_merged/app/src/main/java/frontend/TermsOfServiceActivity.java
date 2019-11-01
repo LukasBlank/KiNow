8a1ac04861354390f0dd1d978072e09cedfe4d4c
Dennis2512 <dennis2598@gmx.net>
2019-11-01 16:56:07 +0100

    Added Terms of Services

---
+++ kinow_merged/app/src/main/java/frontend/TermsOfServiceActivity.java
@@ -0,0 +1,14 @@
+package frontend;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TermsOfServiceActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_of_service);
+    }
+
+}
